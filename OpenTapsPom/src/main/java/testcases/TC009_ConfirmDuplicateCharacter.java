6f261977a5a415004e42fb30aec65c9ed08d9cc6
Karthikeyan G <kkstarcity@gmail.com>
2016-08-30 12:04:29 +0530

    updates

---
+++ OpenTapsPom/src/main/java/testcases/TC009_ConfirmDuplicateCharacter.java
@@ -0,0 +1,13 @@
+package testcases;
+
+public class TC009_ConfirmDuplicateCharacter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Str = "Cognizant";
+		
+
+	}
+
+}
